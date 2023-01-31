5b370f1310eb41777ec37c3d87b10577580a97db
justynalem <lempickajustyna@gmail.com>
2023-01-31 18:12:02 +0100

    use prettier

--- src/components/AddProducts/AddProducts.js
+++ src/components/AddProducts/AddProducts.js
@@ -2,53 +2,59 @@ import React, { useState } from "react";
 import styles from "../../common/styles/Headers.module.scss";
 
 function AddProducts({ addNewProduct, availableProducts }) {
-    const [nazwa, setNazwa] = useState("");
-    const [kategoria, setKategoria] = useState("");
-    const [produktSpozywczy, setProduktSpozywczy] = useState(true);
+  const [nazwa, setNazwa] = useState("");
+  const [kategoria, setKategoria] = useState("");
+  const [produktSpozywczy, setProduktSpozywczy] = useState(true);
 
-    const handleNazwaOnChange = event => {
-        setNazwa(event.target.value);
-    };
-    const handleKategoriaOnChange = event => {
-        setKategoria(event.target.value);
-    };
-    const handleProduktSpozywczy = event => {
-        setProduktSpozywczy(event.target.checked);
-    };
-    const getAndSendNewProduct = () => {
-        const isAlreadyOnList = availableProducts.some(product => product.nazwa === nazwa && product.kategoria === kategoria && product.produktSpozywczy === produktSpozywczy)
-
-        if (isAlreadyOnList) {
-            console.error('Produkt już znajduje się na liście');
-            return
-        }
-        addNewProduct({ nazwa, kategoria, produktSpozywczy });
-    };
-
-    return (
-        <div className={styles.Wrapper}>
-            <label>nazwa produktu</label>
-            <input
-                type='text'
-                value={nazwa.toLowerCase()}
-                onChange={handleNazwaOnChange}></input>
-
-            <label>kategoria produktu</label>
-            <input
-                type='text'
-                value={kategoria.toLowerCase()}
-                onChange={handleKategoriaOnChange}></input>
-            <p>Tylko produkty spożywcze</p>
-            <input
-                type='checkbox'
-                value={produktSpozywczy}
-                onChange={handleProduktSpozywczy}></input>
-
-            <button
-                disabled={!nazwa || !kategoria}
-                onClick={getAndSendNewProduct}> Dodaj </button>
-        </div>
+  const handleNazwaOnChange = event => {
+    setNazwa(event.target.value);
+  };
+  const handleKategoriaOnChange = event => {
+    setKategoria(event.target.value);
+  };
+  const handleProduktSpozywczy = event => {
+    setProduktSpozywczy(event.target.checked);
+  };
+  const getAndSendNewProduct = () => {
+    const isAlreadyOnList = availableProducts.some(
+      product =>
+        product.nazwa === nazwa &&
+        product.kategoria === kategoria &&
+        product.produktSpozywczy === produktSpozywczy
     );
+
+    if (isAlreadyOnList) {
+      console.error("Produkt już znajduje się na liście");
+      return;
+    }
+    addNewProduct({ nazwa, kategoria, produktSpozywczy });
+  };
+
+  return (
+    <div className={styles.Wrapper}>
+      <label>nazwa produktu</label>
+      <input
+        type='text'
+        value={nazwa.toLowerCase()}
+        onChange={handleNazwaOnChange}></input>
+
+      <label>kategoria produktu</label>
+      <input
+        type='text'
+        value={kategoria.toLowerCase()}
+        onChange={handleKategoriaOnChange}></input>
+      <p>Tylko produkty spożywcze</p>
+      <input
+        type='checkbox'
+        value={produktSpozywczy}
+        onChange={handleProduktSpozywczy}></input>
+
+      <button disabled={!nazwa || !kategoria} onClick={getAndSendNewProduct}>
+        {" "}
+        Dodaj{" "}
+      </button>
+    </div>
+  );
 }
 
 export default AddProducts;
