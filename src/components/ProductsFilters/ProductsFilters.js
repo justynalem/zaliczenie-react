2b27e37ff8659d46c578917334647a4e9ccecafd
justynalem <lempickajustyna@gmail.com>
2023-01-31 18:08:45 +0100

    use prettier

--- src/components/ProductsFilters/ProductsFilters.js
+++ src/components/ProductsFilters/ProductsFilters.js
@@ -2,55 +2,56 @@ import React from "react";
 import styles from "../../common/styles/Headers.module.scss";
 import produkty from "../../common/consts/produkty";
 class ProductsFilters extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchPhrase: "",
-            searchCategory: "all",
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchPhrase: "",
+      searchCategory: "all",
+    };
+  }
+  handleChangeSearchPhrase = event => {
+    this.setState({ searchPhrase: event.target.value });
+  };
+
+  handleSelectCategory = event => {
+    this.setState({ searchCategory: event.target.value });
+  };
+
+  filterProducts = () => {
+    const { searchCategory, searchPhrase } = this.state;
+    let filteredProducts = produkty.filter(product =>
+      product.nazwa.includes(searchPhrase.toLowerCase())
+    );
+
+    if (searchCategory !== "all") {
+      filteredProducts = filteredProducts.filter(
+        product => product.kategoria === searchCategory
+      );
     }
-    handleChangeSearchPhrase = event => {
-        this.setState({ searchPhrase: event.target.value });
-    };
+    this.props.sendNameOfProductToApp(filteredProducts);
+  };
 
-    handleSelectCategory = event => {
-        this.setState({ searchCategory: event.target.value });
-    };
-
-    filterProducts = () => {
-        const { searchCategory, searchPhrase } = this.state
-        let filteredProducts = produkty.filter(product =>
-            product.nazwa.includes(searchPhrase.toLowerCase())
-        );
-
-        if (searchCategory !== "all") {
-            filteredProducts = filteredProducts.filter(product => product.kategoria === searchCategory)
-        }
-        this.props.sendNameOfProductToApp(filteredProducts);
-    };
-
-    render() {
-        const categories = produkty.map(product => product.kategoria);
-        const uniqueCategories = [...new Set(categories)].map(category => (
-            <option value={category}>{category}</option>
-        ));
-        return (
-            <div className={styles.Wrapper}>
-                <label>Szukaj wg nazwy produktu:</label>
-                <input
-                    onChange={this.handleChangeSearchPhrase}
-                    type='text'
-                    value={this.state.searchPhrase}></input>{" "}
-                <label> Szukaj wg kategorii:</label>
-                <select onChange={this.handleSelectCategory}>
-                    <option value='all'>wszystkie kategorie</option>
-                    {uniqueCategories}
-                </select>
-                <button onClick={this.filterProducts}>Wyszukaj</button>
-            </div>
-        );
-    }
+  render() {
+    const categories = produkty.map(product => product.kategoria);
+    const uniqueCategories = [...new Set(categories)].map(category => (
+      <option value={category}>{category}</option>
+    ));
+    return (
+      <div className={styles.Wrapper}>
+        <label>Szukaj wg nazwy produktu:</label>
+        <input
+          onChange={this.handleChangeSearchPhrase}
+          type='text'
+          value={this.state.searchPhrase}></input>
+        <label> Szukaj wg kategorii:</label>
+        <select onChange={this.handleSelectCategory}>
+          <option value='all'>wszystkie kategorie</option>
+          {uniqueCategories}
+        </select>
+        <button onClick={this.filterProducts}>Wyszukaj</button>
+      </div>
+    );
+  }
 }
 
 export default ProductsFilters;
-
--- src/components/ShopingList/ShopingList.js
+++ src/components/ShopingList/ShopingList.js
@@ -1,17 +1,20 @@
 import commonColumnsStyles from "../../common/styles/Columns.module.scss";
 
 function ShopingList({ shopingList, deleteProductFromList }) {
-
-  const list = shopingList.map((product, index) => <li onContextMenu={(e) => {
-    e.preventDefault()
-    deleteProductFromList(index)
-
-  }} > {product.nazwa}</li >)
+  const list = shopingList.map((product, index) => (
+    <li
+      onContextMenu={e => {
+        e.preventDefault();
+        deleteProductFromList(index);
+      }}>
+      {product.nazwa}
+    </li>
+  ));
 
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
-        <>{list}</>
+        <ul>{list}</ul>
       </header>
     </div>
   );
