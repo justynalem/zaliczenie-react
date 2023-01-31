07eeecad2fe7dd13f81baef6b459702a81dbb1d5
justynalem <lempickajustyna@gmail.com>
2023-01-31 15:45:38 +0100

    task4

--- src/components/ShopingList/ShopingList.js
+++ src/components/ShopingList/ShopingList.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import commonColumnsStyles from "../../common/styles/Columns.module.scss";
 
 function ShopingList({ shopingList, deleteProductFromShopingList }) {
+  const [crossedOut, setCrossedOut] = useState([]);
+
+  const setProductCrossed = index => {
+    if (crossedOut.includes(index)) {
+      setCrossedOut(crossedOut.filter(el => el !== index));
+    } else {
+      setCrossedOut([...crossedOut, index]);
+    }
+  };
+
   const list = shopingList.map((product, index) => (
     <li
+      style={{
+        textDecoration: crossedOut.some(crossed => crossed === index)
+          ? "line-through"
+          : "auto",
+      }}
+      onClick={e => {
+        deleteProductFromShopingList(index);
+      }}
       onContextMenu={e => {
         e.preventDefault();
-        deleteProductFromShopingList(index);
+        setProductCrossed(index);
       }}>
       {product.nazwa}
     </li>
@@ -14,7 +33,7 @@ function ShopingList({ shopingList, deleteProductFromShopingList }) {
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
-        <>{list}</>
+        <ul>{list}</ul>
       </header>
     </div>
   );
